58af1681948e5774fc2059c0627f8e981565a250
Zack Lalanne <zack.lalanne@gmail.com>
2015-07-26 13:32:04 -0500

    Adding remaining char count

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ var TweetBox = React.createClass({
             <div className="well clearfix">
                 <textarea className="form-control" onChange={this.handleChange}></textarea>
                 <br/>
+                <span>{140 - this.state.text.length}</span>
                 <button className="btn btn-primary pull-right" disabled={this.state.text.length === 0}>Tweet</button>
             </div>
         );
