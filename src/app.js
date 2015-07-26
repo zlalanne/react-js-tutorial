d6a1655a78ccc06abafaf4e906d9d7da5cb3281e
Zack Lalanne <zack.lalanne@gmail.com>
2015-07-26 13:29:14 -0500

    Working react implementation

---
+++ src/app.js
@@ -0,0 +1,24 @@
+var TweetBox = React.createClass({
+    getInitialState: function() {
+        return {
+            text: ""
+        };
+    },
+    handleChange: function(event) {
+        this.setState({ text: event.target.value });
+    },
+    render: function() {
+        return (
+            <div className="well clearfix">
+                <textarea className="form-control" onChange={this.handleChange}></textarea>
+                <br/>
+                <button className="btn btn-primary pull-right" disabled={this.state.text.length === 0}>Tweet</button>
+            </div>
+        );
+    }
+});
+
+React.render(
+    <TweetBox />,
+    document.body
+);
